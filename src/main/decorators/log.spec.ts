8637a8ec4c40fc320c9e7fb9a904e937bf84787a
Patrick Delfim <patrickdelfim@gmail.com>
2022-05-30 08:59:21 -0300

    test: ensure LogControllerDecorator returns the same result of the controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -46,4 +46,25 @@ describe('LogController Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  test('should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse: HttpResponse = {
+      statusCode: 200,
+      body: {
+        name: 'patrick'
+      }
+    }
+    const response = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(response)
+  })
 })
