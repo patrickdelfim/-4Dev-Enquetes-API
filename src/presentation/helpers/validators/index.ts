e140e1865309d6962158fdf1fa7730c4590ff08b
Patrick Delfim <patrickdelfim@gmail.com>
2022-06-02 22:27:42 -0300

    test: ensure RequiredFieldValidation returns a MissingParamsError if validation fails

---
+++ src/presentation/helpers/validators/index.ts
@@ -0,0 +1,1 @@
+export * from './requiredFieldValidation/required-field-validation'
--- src/presentation/helpers/validators/requiredFieldValidation/required-field-validation.ts
+++ src/presentation/helpers/validators/requiredFieldValidation/required-field-validation.ts
@@ -1,5 +1,5 @@
-import { MissingParamError } from '../../errors'
-import { Validation } from './validation'
+import { MissingParamError } from '../../../errors'
+import { Validation } from '../validation'
 
 export class RequiredFieldValidation implements Validation {
   constructor (private readonly fieldName: string) {}
