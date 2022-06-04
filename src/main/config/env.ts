2333e6f85f8fb1f771d2e254e03f2b6ad87af5d4
Patrick Delfim <patrickdelfim@gmail.com>
2022-06-04 20:31:07 -0300

    feat: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
