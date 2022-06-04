fb52c2a5c18410c197a36f213cd4e7bac3722839
Patrick Delfim <patrickdelfim@gmail.com>
2022-06-04 19:49:00 -0300

    refactor: improve file names

--- src/main/factories/signup/signup-validation-factory.spec.ts
+++ src/main/factories/signup/signup-validation-factory.spec.ts
@@ -1,7 +1,7 @@
 import { RequiredFieldValidation, CompareFieldsValidation, ValidationComposite, EmailValidation } from '../../../presentation/helpers/validators'
 import { Validation } from '../../../presentation/protocols/validation'
 import { EmailValidator } from '../../../presentation/protocols/emailValidator'
-import { makeSignUpValidation } from './signup-validation'
+import { makeSignUpValidation } from './signup-validation-factory'
 
 jest.mock('../../../presentation/helpers/validators/validationComposite/validation-composite')
 
--- src/main/routes/signup/signup-routes.ts
+++ src/main/routes/signup/signup-routes.ts
@@ -1,6 +1,6 @@
 import { RequestHandler, Router } from 'express'
-import { adaptRoute } from '../adapters/express-route-adapter'
-import { makeSignUpController } from '../factories/signup/signup'
+import { adaptRoute } from '../../adapters/express-route-adapter'
+import { makeSignUpController } from '../../factories/signup/signup'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()) as RequestHandler)
