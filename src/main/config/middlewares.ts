cf552b92f49ff56118ce2262cbbf5899c72660c0
Patrick Delfim <patrickdelfim@gmail.com>
2022-06-04 21:36:40 -0300

    feat: add body parser error handler middleware

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,9 @@
 import { Express } from 'express'
+import bodyParserErrorHandler from 'express-body-parser-error-handler'
 import { bodyParser, contentType, cors } from '../middlewares'
-
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(bodyParserErrorHandler())
   app.use(cors)
   app.use(contentType)
 }
