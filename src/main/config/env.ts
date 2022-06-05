d83a81bc19c7fa43a06e8f0510de869472c6793f
Patrick Delfim <patrickdelfim@gmail.com>
2022-06-05 14:37:35 -0300

    chore: update mongo env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H'
 }
