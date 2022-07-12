4c6d9648fab5ae6d4eaff2da20931cad207a860f
Patrick Delfim <patrickdelfim@gmail.com>
2022-07-11 22:47:31 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -1,6 +1,10 @@
 import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-mongo-repository'
+
+// file deepcode ignore HardcodedNonCryptoSecret: Test Purpose
+// file deepcode ignore NoHardcodedPasswords:  Test Purpose
+
 describe('Account Mongo Repository', () => {
   let accountCollection: Collection
 
@@ -75,6 +79,24 @@ describe('Account Mongo Repository', () => {
       const account = await accountCollection.findOne({ _id: id })
       expect(account).toBeTruthy()
       expect(account.accessToken).toBe('any_token')
+    })
+  })
+
+  describe('loadByToken()', () => {
+    test('should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
   })
 })
