2ec2a9a0c65eeff3c99d8d2ac7e3d3059fef59c9
Patrick Delfim <patrickdelfim@gmail.com>
2022-08-01 21:55:54 -0300

    refactor: move docs to separate files

---
+++ src/main/docs/components.ts
@@ -0,0 +1,15 @@
+import { badRequest, notFound, serverError, unauthorized, forbidden } from './components/'
+import { apiKeyAuthSchema } from './schemas/'
+
+export default {
+
+  securitySchemes: {
+    apiKeyAuth: apiKeyAuthSchema
+  },
+  badRequest,
+  unauthorized,
+  serverError,
+  notFound,
+  forbidden
+
+}
---
+++ src/main/docs/schemas.ts
@@ -0,0 +1,27 @@
+
+import {
+  errorSchema,
+  accountSchema,
+  loginParamsSchema,
+  surveyAnswerSchema,
+  surveySchema,
+  surveysSchema,
+  signUpParamsSchema,
+  addSurveyParamsSchema,
+  saveSurveyParamsSchema,
+  surveyResultSchema
+} from './schemas/'
+
+export default {
+  account: accountSchema,
+  loginParams: loginParamsSchema,
+  signUpParams: signUpParamsSchema,
+  addSurveyParams: addSurveyParamsSchema,
+  error: errorSchema,
+  surveys: surveysSchema,
+  survey: surveySchema,
+  surveyAnswer: surveyAnswerSchema,
+  saveSurveyParams: saveSurveyParamsSchema,
+  surveyResult: surveyResultSchema
+
+}
