afa583a8677e2e243fed5bb2a086209f549d74b9
Patrick Delfim <patrickdelfim@gmail.com>
2022-08-03 21:57:08 -0300

    docs: update save survey result docs

--- src/main/docs/schemas.ts
+++ src/main/docs/schemas.ts
@@ -9,7 +9,8 @@ import {
   signUpParamsSchema,
   addSurveyParamsSchema,
   saveSurveyParamsSchema,
-  surveyResultSchema
+  surveyResultSchema,
+  surveyResultAnswerSchema
 } from './schemas/'
 
 export default {
@@ -22,6 +23,6 @@ export default {
   survey: surveySchema,
   surveyAnswer: surveyAnswerSchema,
   saveSurveyParams: saveSurveyParamsSchema,
-  surveyResult: surveyResultSchema
-
+  surveyResult: surveyResultSchema,
+  surveyResultAnswer: surveyResultAnswerSchema
 }
